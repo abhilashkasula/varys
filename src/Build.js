c381ba2b745995d78ddbafbf553d219166a1f639
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2021-01-01 21:36:26 +0530

    Extract functions for success and failure datasets

--- src/Build.js
+++ src/Build.js
@@ -2,6 +2,50 @@ import {useState, createRef, useEffect} from 'react';
 import styled from 'styled-components';
 import {Line} from 'react-chartjs-2';
 
+const getSuccessDataset = (data) => ({
+  label: 'Success',
+  fill: true,
+  lineTension: 0.1,
+  backgroundColor: 'rgba(75,192,192,0.4)',
+  borderColor: 'rgba(75,192,192,1)',
+  borderCapStyle: 'butt',
+  borderDash: [],
+  borderDashOffset: 0.0,
+  borderJoinStyle: 'miter',
+  pointBorderColor: 'rgba(75,192,192,1)',
+  pointBackgroundColor: '#fff',
+  pointBorderWidth: 1,
+  pointHoverRadius: 5,
+  pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+  pointHoverBorderColor: 'rgba(220,220,220,1)',
+  pointHoverBorderWidth: 2,
+  pointRadius: 1,
+  pointHitRadius: 10,
+  data,
+});
+
+const getFailedDataset = (data) => ({
+  label: 'Failed',
+  fill: true,
+  lineTension: 0.1,
+  backgroundColor: 'rgba(1000,0,0,0.4)',
+  borderColor: 'rgba(1000,0,0,1)',
+  borderCapStyle: 'butt',
+  borderDash: [],
+  borderDashOffset: 0.0,
+  borderJoinStyle: 'miter',
+  pointBorderColor: 'rgba(1000,0,0,1)',
+  pointBackgroundColor: '#fff',
+  pointBorderWidth: 1,
+  pointHoverRadius: 5,
+  pointHoverBackgroundColor: 'rgba(1000,0,0,1)',
+  pointHoverBorderColor: 'rgba(1000,0,0,1)',
+  pointHoverBorderWidth: 2,
+  pointRadius: 1,
+  pointHitRadius: 10,
+  data,
+});
+
 const Build = ({className}) => {
   const [data, setData] = useState(null);
 
@@ -17,48 +61,8 @@ const Build = ({className}) => {
     return {
       labels: labels.map(([date, month]) => `${date}/${month}`),
       datasets: [
-        {
-          label: 'Success',
-          fill: true,
-          lineTension: 0.1,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(75,192,192,1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: succeededJobs.map((success) => success.length),
-        },
-        {
-          label: 'Failed',
-          fill: true,
-          lineTension: 0.1,
-          backgroundColor: 'rgba(1000,0,0,0.4)',
-          borderColor: 'rgba(1000,0,0,1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(1000,0,0,1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(1000,0,0,1)',
-          pointHoverBorderColor: 'rgba(1000,0,0,1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: failedJobs.map((success) => success.length),
-        },
+        getSuccessDataset(succeededJobs.map((success) => success.length)),
+        getFailedDataset(failedJobs.map((fail) => fail.length)),
       ],
     };
   };
