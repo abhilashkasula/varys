f31e3352b0f341442bbaf5470d36ede589e0e90a
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2021-01-14 17:22:05 +0530

    Add graph for average duration for builds per day

---
+++ src/AverageDuration.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import {Bar} from 'react-chartjs-2';
+
+const getAverageDurationData = ({labels, averageDurations}) => {
+  return {
+    labels: labels.map(([date, month]) => `${date}/${month}`),
+    datasets: [
+      {
+        label: 'Duration',
+        fill: true,
+        lineTension: 0.1,
+        barThickness: 25,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderWidth: 1.5,
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: averageDurations,
+      },
+    ],
+  };
+};
+
+const AverageDuration = ({className, data}) => {
+  console.log(data)
+  return (
+    <div className={className}>
+      <Bar data={getAverageDurationData(data)} width={40} height={20} />
+    </div>
+  );
+};
+
+const StyledAverageDuration = styled(AverageDuration)`
+  width: 370px;
+`;
+
+export default StyledAverageDuration;
--- src/Build.js
+++ src/Build.js
@@ -1,85 +1,24 @@
-import {useState, createRef, useEffect} from 'react';
-import styled from 'styled-components';
-import {Line} from 'react-chartjs-2';
+import {useState, useEffect} from 'react';
+import DailyBuilds from './DailyBuilds';
+import AverageDuration from './AverageDuration';
 
-const getSuccessDataset = (data) => ({
-  label: 'Success',
-  fill: true,
-  lineTension: 0.1,
-  backgroundColor: 'rgba(75,192,192,0.4)',
-  borderColor: 'rgba(75,192,192,1)',
-  borderCapStyle: 'butt',
-  borderDash: [],
-  borderDashOffset: 0.0,
-  borderJoinStyle: 'miter',
-  pointBorderColor: 'rgba(75,192,192,1)',
-  pointBackgroundColor: '#fff',
-  pointBorderWidth: 1,
-  pointHoverRadius: 5,
-  pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-  pointHoverBorderColor: 'rgba(220,220,220,1)',
-  pointHoverBorderWidth: 2,
-  pointRadius: 1,
-  pointHitRadius: 10,
-  data,
-});
-
-const getFailedDataset = (data) => ({
-  label: 'Failed',
-  fill: true,
-  lineTension: 0.1,
-  backgroundColor: 'rgba(1000,0,0,0.4)',
-  borderColor: 'rgba(1000,0,0,1)',
-  borderCapStyle: 'butt',
-  borderDash: [],
-  borderDashOffset: 0.0,
-  borderJoinStyle: 'miter',
-  pointBorderColor: 'rgba(1000,0,0,1)',
-  pointBackgroundColor: '#fff',
-  pointBorderWidth: 1,
-  pointHoverRadius: 5,
-  pointHoverBackgroundColor: 'rgba(1000,0,0,1)',
-  pointHoverBorderColor: 'rgba(1000,0,0,1)',
-  pointHoverBorderWidth: 2,
-  pointRadius: 1,
-  pointHitRadius: 10,
-  data,
-});
-
-const Build = ({className}) => {
+const Build = () => {
   const [data, setData] = useState(null);
-
-  const ref = createRef();
 
   useEffect(() => {
     fetch('/api/build/perDayStats')
       .then((res) => res.json())
       .then((data) => setData(() => data));
-  });
+  }, []);
 
-  const getBuildData = ({labels, succeededJobs, failedJobs}) => {
-    return {
-      labels: labels.map(([date, month]) => `${date}/${month}`),
-      datasets: [
-        getSuccessDataset(succeededJobs.map((success) => success.length)),
-        getFailedDataset(failedJobs.map((fail) => fail.length)),
-      ],
-    };
-  };
-
-  return (
-    <div className={className}>
-      {data ? (
-        <Line data={getBuildData(data)} ref={ref} width={40} height={20} />
-      ) : (
-        'Loading..'
-      )}
+  return data ? (
+    <div>
+      <DailyBuilds data={data} />
+      <AverageDuration data={data} />
     </div>
+  ) : (
+    <p>Loading...</p>
   );
 };
 
-const StyledBuild = styled(Build)`
-  width: 370px;
-`;
-
-export default StyledBuild;
+export default Build;
